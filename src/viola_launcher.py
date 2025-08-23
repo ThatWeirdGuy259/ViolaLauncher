db5650f4924b8135ac30379350dd17a941fef49b
Flynn <thatweirdguy259l@example.com>
2025-08-23 19:56:03 +1000

    First beta

--- src/viola_launcher.py
+++ src/viola_launcher.py
@@ -257,7 +257,7 @@ class SettingsPage(QWidget):
 
 # ---------------- Main Launcher ----------------
 class ViolaLauncher(QWidget):
-    CURRENT_VERSION = "1.0.27"
+    CURRENT_VERSION = "1.0.0"
 
     def __init__(self):
         super().__init__()
